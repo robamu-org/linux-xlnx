798c1140b9fb7415e74f5d3fda65df8058bf4ebe
William Bourque <wfb@xiphos.com>
2016-12-16 13:36:36 -0500

    drivers/spi/spidev.c : Fix 'spidev' compatible
    New kernel Oops if a device tree use 'compatible = "spidev"' line
    It was an intended changes by the kernel developers:
     https://patchwork.kernel.org/patch/6113191
    
    However, no one bothered to develop "correct ways" to use spidev.
    Right now, the only way is to patch the kernel to add a new
     chip as 'compatible = '.
    It is bothersome as we want to be able to be able to support any
     SPI chip on daughter cards without having to patch the kernel
     for each of those. Patching would be especially hazardous as the
     card could be developed by a customer, not directly by us.
    
    It seems Rasberry Pi has the same issue as us about that:
     https://github.com/raspberrypi/linux/issues/1054
    
    This commit does 2 things:
    - Add 'compatible = "adi,ad7689"' for the PIM ADC that we know need
      to use spidev. This support our ADC.
    - Add back 'compatible = "spidev"' back in spidev so that future
      card and customer can get to talk to spi devices without having to
      patch the kernel.

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -695,6 +695,8 @@ static struct class *spidev_class;
 static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "rohm,dh2228fv" },
 	{ .compatible = "lineartechnology,ltc2488" },
+	{ .compatible = "adi,ad7689" },
+	{ .compatible = "spidev" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
