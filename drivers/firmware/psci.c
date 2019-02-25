58b49bc01c9a0814156151d8d772238c3b797331
Liam Beguin <lvb@xiphos.com>
2019-02-25 15:19:39 -0500

    drivers: firmware: psci: don't register arm_pm_restart
    Don't set `arm_pm_restart` when CONFIG_POWER_RESET_GPIO_RESTART is
    enabled. defining `arm_pm_restart` prevents all other restart handlers
    from being registered.
    
    Signed-off-by: Liam Beguin <lvb@xiphos.com>

--- drivers/firmware/psci.c
+++ drivers/firmware/psci.c
@@ -232,10 +232,12 @@ static int get_set_conduit_method(struct device_node *np)
 	return 0;
 }
 
+#if !defined(CONFIG_POWER_RESET_GPIO_RESTART)
 static void psci_sys_reset(enum reboot_mode reboot_mode, const char *cmd)
 {
 	invoke_psci_fn(PSCI_0_2_FN_SYSTEM_RESET, 0, 0, 0);
 }
+#endif
 
 #if !defined(CONFIG_POWER_RESET_GPIO)
 static void psci_sys_poweroff(void)
@@ -515,7 +517,9 @@ static void __init psci_0_2_set_functions(void)
 
 	psci_ops.migrate_info_type = psci_migrate_info_type;
 
+#if !defined(CONFIG_POWER_RESET_GPIO_RESTART)
 	arm_pm_restart = psci_sys_reset;
+#endif
 
 #if !defined(CONFIG_POWER_RESET_GPIO)
 	pm_power_off = psci_sys_poweroff;
